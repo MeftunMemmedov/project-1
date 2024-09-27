7ec846edf2389a275fcc4ec65844d4300a55fc9a
MFTNWebMusician <mftnmusic6386@gmail.com>
2024-09-27 16:42:15 +0400

    commit-1

---
+++ app.js
@@ -0,0 +1,26 @@
+var num = 0;
+var arr = [
+    {
+        id: 1,
+        name: 'user1'
+    },
+    {
+        id: 2,
+        name: 'user2'
+    }
+];
+var user = [
+    {
+        id: 1,
+        name: 'user1'
+    },
+    {
+        id: 2,
+        name: 'user2'
+    }
+];
+console.log(num);
+function showConsole(message) {
+    console.log(message);
+}
+showConsole('Hey');
---
+++ app.ts
@@ -0,0 +1,30 @@
+let num:number=0
+let arr:object[]=[
+    {
+        id:1,
+        name:'user1'
+    },
+    {
+        id:2,
+        name:'user2'
+    }
+]
+
+let user:{id:number, name:string}[]=[
+    {
+        id:1,
+        name:'user1'
+    },
+    {
+        id:2,
+        name:'user2'
+    }
+]
+
+console.log(num)
+
+function showConsole(message:string){
+    console.log(message)
+}
+
+showConsole('Hey')
